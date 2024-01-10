c8e5ea140b7124cd5d10ad1f0a9ea012a3552f51
aline santos <alinizi@hotmail.com>
2024-01-10 11:19:18 -0300

    finalizando desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+﻿﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("================= Meu Iphone ====================");
+System.Console.WriteLine();
+Iphone meuIPhone = new Iphone("424234235", "Iphone 14", "346345353", 64);
+meuIPhone.InstalarAplicativo("Twitter");
+
+System.Console.WriteLine();
+
+System.Console.WriteLine("================= Meu Nokia ====================");
+System.Console.WriteLine();
+Nokia meuNokia = new Nokia("4254434353", "Nokia Antigo", "r4325235346", 32);
+meuNokia.InstalarAplicativo("Snake");
